684297f150feebf3a0cd018a7d644f8d674cbb3d
Nick Abbott <nick.abbott@hitachivantara.com>
2021-04-09 12:58:48 +0100

    D147147: Create some basic Ansible modules for HNAS
    Install script modified to check ansible version is a minimum value.  Then try to get the ansible module location from ansible - if not, try to work out which version of python is being used, and then work out where to store the files.  Variable names also changed to lower case.  Not been tested on an ansible install using Python3 yet.
    
    __init__.py:
    install.sh:

--- install.sh
+++ install.sh
@@ -1,66 +1,115 @@
 #!/bin/bash
+
+# Function to compare two version strings https://stackoverflow.com/a/24067243/9761984
+function versionGreaterThan() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+# param 1 = fixed version for comparison
+# param 2 = one to check
+# returns 0 if the supplied version is NOT greater than the fixed version
+function checkVersion()
+{
+    version=`echo "$2" | cut -d' ' -f2`
+    if versionGreaterThan "$1" "$version"; then
+        return 0
+    fi
+    return 1
+}
+
+ansible_minimum_version="2.9"
+
 # should check for ansible and check version is 2.9 or above
+# should be of the form "ansible 2.9.10"
+ansible_version=`ansible --version | head -1`
+echo ${ansible_version}
 
-ANSIBLE_VERSION=`ansible --version | head -1`
-echo ${ANSIBLE_VERSION}
-
-# get python folder
-PYTHON_PACKAGES=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
-echo "Python package location:"
-echo "  PYTHON_PACKAGES=\"${PYTHON_PACKAGES}\""
-
-if [ "${PYTHON_PACKAGES}" == "" ]; then
-    echo "Failed to find Python package install location, installation aborted"
+if checkVersion "${ansible_minimum_version}" "${ansible_version}"; then
+    echo "Ansible version must be ${ansible_minimum_version} or newer - installation aborted"
     exit 1
 fi
 
-# should check for module_utils
-echo "Ansible module locations:"
-ANSIBLE_MODULE_UTILS="${PYTHON_PACKAGES}/ansible/module_utils"
-ANSIBLE_MOUDLES="${PYTHON_PACKAGES}/ansible/modules"
-echo "  ANSIBLE_MODULE_UTILS=\"${ANSIBLE_MODULE_UTILS}\""
-echo "  ANSIBLE_MOUDLES=\"${ANSIBLE_MOUDLES}\""
+# should show where the ansible modules are stored
+# should be of the form "  ansible python module location = /usr/lib/python2.7/site-packages/ansible"
+ansible_python_packages=`ansible --version | grep "python module location"`
+python_packages=`echo "${ansible_python_packages}" | cut -d'=' -f2`
+# removes spaces at the beginning of the variable
+python_packages=`echo ${python_packages}`
+#python_packages=""
 
-if [ ! -d "${ANSIBLE_MODULE_UTILS}" ] || [ ! -d  "${ANSIBLE_MOUDLES}" ]; then
+if [ "${python_packages}" == "" ]; then
+# unable to determine location of pyton packages Ansible is using, so try to work it out, including which version of Python is being used
+    echo "Unable to get the Ansible python module location, attempting to work it out.."
+    # should be of the form "  python version = 2.7.5 (default, Apr  2 2020, 13:16:51) [GCC 4.8.5 20150623 (Red Hat 4.8.5-39)]"
+    ansible_python_version=`ansible --version | grep "python version"`
+#ansible_python_version="  python version = 3.1.5 (default, Apr  2 2020, 13:16:51) [GCC 4.8.5 20150623 (Red Hat 4.8.5-39)]"
+    echo ${ansible_python_version}
+    python_version=`echo "${ansible_python_version}" | cut -d'=' -f2`
+
+    if checkVersion "3" "${python_version}"; then
+        echo "  Ansible using python version 2"
+        python_packages=`python2 -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
+    else
+        echo "  Ansible using python version 3"
+        python_packages=`python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'`
+    fi
+    python_packages="${python_packages}/ansible"
+fi
+
+echo "Python package location:"
+echo "  python_packages=\"${python_packages}\""
+
+if [ "${python_packages}" == "" ]; then
+    echo "Failed to find Python package install location - installation aborted"
+    exit 1
+fi
+
+# should check for existancce of the Ansible module and module_utils folders
+echo "Ansible module locations:"
+ansible_module_utils="${python_packages}/module_utils"
+ansible_modules="${python_packages}/modules"
+echo "  ansible_module_utils=\"${ansible_module_utils}\""
+echo "  ansible_modules=\"${ansible_modules}\""
+
+if [ ! -d "${ansible_module_utils}" ] || [ ! -d  "${ansible_modules}" ]; then
     echo "Failed to find Ansible module location, installation aborted"
     exit 1
 fi
 
-HV_MODULE_UTILS="${ANSIBLE_MODULE_UTILS}/storage/hv"
-HV_MODULES="${ANSIBLE_MOUDLES}/storage/hv"
-if [ ! -d "${HV_MODULES}" ]; then
-    echo "Creating ${HV_MODULES}"
-    mkdir -p "${HV_MODULES}"
+# If HV specific folders don't exist, create them
+hv_module_utils="${ansible_module_utils}/storage/hv"
+hv_modules="${ansible_modules}/storage/hv"
+if [ ! -d "${hv_modules}" ]; then
+    echo "Creating ${hv_modules}"
+    mkdir -p "${hv_modules}"
 fi
 
-if [ ! -d "${HV_MODULE_UTILS}" ]; then
-    echo "Creating ${HV_MODULE_UTILS}"
-    mkdir "${HV_MODULE_UTILS}"
+if [ ! -d "${hv_module_utils}" ]; then
+    echo "Creating ${hv_module_utils}"
+    mkdir "${hv_module_utils}"
 fi
 
-# number of modules to be installed
-MODULE_COUNT=5
+common_files=("hv_hnas_main.py" "__init__.py")
 
-echo -e "\nInstalling common module(s)"
-\cp -f hv_hnas_main.py ${HV_MODULE_UTILS}
-touch ${HV_MODULE_UTILS}/__init__.py
+echo -e "\nInstalling common modules (${#common_files[@]} files) to ${hv_module_utils}"
+for file in "${common_files[@]}"; do
+    \cp -f "$file" "${hv_module_utils}"
+done
 
-echo "Installing Hitachi NAS Modules"
-\cp -f hv_hnas_facts.py ${HV_MODULES}
-\cp -f hv_hnas_filesystem.py ${HV_MODULES}
-\cp -f hv_hnas_share_export.py ${HV_MODULES}
-\cp -f hv_hnas_storage_pool.py ${HV_MODULES}
-\cp -f hv_hnas_virtual_server.py ${HV_MODULES}
-touch ${HV_MODULES}/__init__.py
+module_files=("hv_hnas_facts.py" "hv_hnas_filesystem.py" "hv_hnas_share_export.py" "hv_hnas_storage_pool.py" "hv_hnas_virtual_server.py" "__init__.py")
 
-INSTALLED_MODULES=`ansible-doc -l | grep hv_hnas`
+echo "Installing Hitachi NAS Modules (${#module_files[@]} files) to ${hv_modules}"
+for file in "${module_files[@]}"; do
+    \cp -f "$file" "${hv_modules}"
+done
+
+installed_modules=`ansible-doc -l | grep hv_hnas`
 echo "Installed hv_hnas* modules:"
-echo "${INSTALLED_MODULES}"
+echo "${installed_modules}"
 
-INSTALLED_MODULE_COUNT=$(wc -l <<< "${INSTALLED_MODULES}")
+installed_module_count=$(wc -l <<< "${installed_modules}")
 
-if [[ $INSTALLED_MODULE_COUNT == 5 ]]; then
-    echo "All Hitachi NAS Modules for Ansible successfully installed"
+# Check all modules were installed - 1 less than show in the ansible-doc command (__init__.py does not count)
+if [[ $installed_module_count == $(( ${#module_files[@]} - 1 )) ]]; then
+    echo -e "\nAll Hitachi NAS Modules for Ansible successfully installed"
 fi
 
 exit 0
